9616ab8d3cdcbc8a0c48998f675a032f6b427d94
igor viana <igor.viana560@gmail.com>
2024-05-04 11:33:07 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]')
+
+    const heroSection = document.querySelector('.hero')
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if( posicaoAtual < alturaHero){
+            ocultaElementoDoHeader();
+        } else {
+            mostraElementoDoHeader();
+        }
+    })
     
+    //Seção de atrações, programação das abas.
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton;
@@ -11,7 +26,30 @@ document.addEventListener('DOMContentLoaded', function(){
             botao.target.classList.add('shows__tabs__button--is-active')
         })
     }
+
+//Seção Faq accordions
+    for (let i = 0; i < questions.length; i++) { 
+        questions[i].addEventListener('click', abreOufechaResposta);
+    
+    }
 })
+
+function ocultaElementoDoHeader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+function mostraElementoDoHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+
+function abreOufechaResposta (elemento){
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
